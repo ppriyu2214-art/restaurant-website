9cfad5f3d1749bb65a5fbe33ad681c1cc8520092
Priyanka Gaikwad <ppriyu2214@gmail.com>
2026-02-27 09:28:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Scroll to Menu
+function scrollToMenu() {
+    document.getElementById("menu").scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+// Open Instagram
+function openInstagram() {
+    window.open("https://instagram.com", "_blank");
+}
