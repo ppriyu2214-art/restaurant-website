8def9cb491ef583c3a9240c7eaf919f61a635ef9
Priyanka Gaikwad <ppriyu2214@gmail.com>
2026-02-27 10:17:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-// Scroll to Menu
-function scrollToMenu() {
-    document.getElementById("menu").scrollIntoView({
-        behavior: "smooth"
-    });
-}
-
-// Open Instagram
-function openInstagram() {
+// Instagram Button
+function visitInstagram(){
     window.open("https://instagram.com", "_blank");
 }
+
+// Scroll Animation
+const faders = document.querySelectorAll(".fade-in");
+
+window.addEventListener("scroll", () => {
+    faders.forEach(el => {
+        const position = el.getBoundingClientRect().top;
+        if(position < window.innerHeight - 100){
+            el.classList.add("show");
+        }
+    });
+});
